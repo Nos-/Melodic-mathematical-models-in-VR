f4f82e7bda554e81b5dca4d5082b7aca4b18ad6e
Nos- <Nos-@users.noreply.github.com>
2022-05-02 00:21:05 +0200

    Update project to fix some bugs

--- Klingende_Mathemodelle_in_VR/Fadenmodell_12_4.gd
+++ Klingende_Mathemodelle_in_VR/Fadenmodell_12_4.gd
@@ -1,5 +1,6 @@
 extends Spatial
 
+var direction = Vector3.ZERO
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -8,16 +9,52 @@ extends Spatial
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	direction.y = PI/180
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	rotate_x(direction.x)
+	rotate_y(direction.y)
+	rotate_z(direction.z)
 
 
 func _on_restart_pressed():
-	$Shape_IndexedFaceSet002/Play_2/AudioStreamPlayer_2.stop()
-	$Shape_IndexedFaceSet003/Play_3/AudioStreamPlayer_3.stop()
-	$Shape_IndexedFaceSet004/Play_4/AudioStreamPlayer_4.stop()
-	$Shape_IndexedFaceSet005/Play_5/AudioStreamPlayer_5.stop()
+	$Shape_IndexedFaceSet002/AudioStreamPlayer_2.stop()
+	$Shape_IndexedFaceSet003/AudioStreamPlayer_3.stop()
+	$Shape_IndexedFaceSet004/AudioStreamPlayer_4.stop()
+	$Shape_IndexedFaceSet005/AudioStreamPlayer_5.stop()
+
+	direction = Vector3.ZERO
+
+
+func _on_rotate_right_pressed():
+	direction.y += PI/180
+
+
+func _on_rotate_left_pressed():
+	direction.y -= PI/180
+
+
+func _on_rotate_up_pressed():
+	direction.z += PI/180
+
+
+func _on_rotate_down_pressed():
+	direction.z -= PI/180
+
+
+func _on_play_4_pressed():
+	$Shape_IndexedFaceSet004/AudioStreamPlayer_4.play()
+
+
+func _on_play_2_pressed():
+	$Shape_IndexedFaceSet002/AudioStreamPlayer_2.play()
+
+
+func _on_play_3_pressed():
+	$Shape_IndexedFaceSet003/AudioStreamPlayer_3.play()
+
+
+func _on_play_5_pressed():
+	$Shape_IndexedFaceSet005/AudioStreamPlayer_5.play()
